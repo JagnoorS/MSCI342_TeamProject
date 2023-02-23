d3635109b7f8d25d453c5b3230392f3e258237be
“JagnoorS” <js3sangh@uwaterloo.ca>
2023-02-23 17:23:07 -0500

    Added manager's employee logged hours

---
+++ client/src/components/M_EmployeeLoggedHours/index.js
@@ -0,0 +1,29 @@
+import { Link, Typography } from '@material-ui/core';
+import React, { Component } from 'react';
+// import Link from "@material-ui/core/Link";
+import history from "../Navigation/history"
+
+
+const M_EmployeeLoggedHours = () =>  {
+
+    return (
+        <div>
+            <Typography variant = 'h3' colors = 'black' >
+                Welcome Manager's to employee logged hours
+            </Typography>
+
+            <Typography variant = 'h5' colors = 'darkblue' >
+              
+            </Typography>
+
+            <Typography variant = 'h6' colors = 'darkblue' >
+               
+            </Typography>
+
+
+        </div>
+        
+    );
+}
+
+export default M_EmployeeLoggedHours;
--- client/src/components/Navigation/PrivateRoute.js
+++ client/src/components/Navigation/PrivateRoute.js
@@ -5,18 +5,20 @@ import history from './history';
 import Landing from "../Landing"
 import ManagerLanding from "../ManagerLanding"
 import EmployeeLanding from "../EmployeeLanding"
+import M_EmployeeLoggedHours from "../M_EmployeeLoggedHours"
 
 export default function PrivateRoute({
   //authenticated,
   //...rest
 }) {
   return (
-
+    
     <Router history={history}>
       <Switch>
       <Route path="/" exact component={Landing} />
       <Route path="/ManagerLanding" exact component={ManagerLanding} />
       <Route path="/EmployeeLanding" exact component={EmployeeLanding} />
+      <Route path="/M_EmployeeLoggedHours" exact component={M_EmployeeLoggedHours} />
       </Switch>
     </Router>
   );
