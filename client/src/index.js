a56a8c21f2ea4650b7ae4ebb9666db8d3fe1f62d
ag3722 <90345401+ag3722@users.noreply.github.com>
2023-02-23 14:20:24 -0500

    Test2 Message

--- client/src/index.js
+++ client/src/index.js
@@ -11,6 +11,7 @@ ReactDOM.render(
   document.getElementById("root")
 );
 //test
+// test2 - 23
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
