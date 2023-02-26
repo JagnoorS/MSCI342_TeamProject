6541c83bfa73c99d45aff528f92c50b71adabdf0
ag3722 <90345401+ag3722@users.noreply.github.com>
2023-02-25 20:10:19 -0500

    Added SalesEntry

---
+++ client/src/components/SalesEntry/index.js
@@ -0,0 +1,29 @@
+import { Link, Typography } from '@material-ui/core';
+import React, { Component } from 'react';
+// import Link from "@material-ui/core/Link";
+import history from "../Navigation/history"
+
+
+const SalesEntry = () =>  {
+
+    return (
+        <div>
+            <Typography variant = 'h3' colors = 'black' >
+                Creating payroll
+            </Typography>
+
+            <Typography variant = 'h5' colors = 'darkblue' >
+                
+            </Typography>
+
+            <Typography variant = 'h6' colors = 'darkblue' >
+               
+            </Typography>
+
+
+        </div>
+        
+    );
+}
+
+export default SalesEntry;
