fe15eacad3227d028a2621d6aa5f92338c18158b
ag3722 <90345401+ag3722@users.noreply.github.com>
2023-02-28 22:38:40 -0500

    addedBackendSalesEntryPage

--- client/src/components/SalesEntry/index.js
+++ client/src/components/SalesEntry/index.js
@@ -13,6 +13,9 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
+
+const serverURL = ""; //enable for dev mode
+
 function Copyright(props) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
@@ -32,11 +35,50 @@ export default function SignUp() {
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
   };
+
+const [reportingDate, setReportingDate] = React.useState("");
+const [regularVolume, setRegularVolume] = React.useState("");
+const [regularPurchasePrice, setRegularPurchasePrice] = React.useState("");
+const [regularSellPrice, setRegularSellPrice] = React.useState("");
+const [mid_gradeVolume, setMid_GradeVolume] = React.useState("");
+const [mid_gradePurchasePrice, setMid_GradePurchasePrice] = React.useState("");
+const [mid_gradeSellPrice, setMid_GradeSellPrice] = React.useState("");
+const [premiumVolume, setPremiumVolume] = React.useState("");
+const [premiumPurchasePrice, setPremiumPurchasePrice] = React.useState("");
+const [premiumSellPrice, setPremiumSellPrice] = React.useState("");
+
+const salesEntry = {
+  reportingDate: reportingDate,
+  regularVolume: regularVolume,
+  regularPurchasePrice: regularPurchasePrice,
+  regularSellPrice: regularSellPrice,
+  regularNetProfit: (regularSellPrice - regularPurchasePrice) * regularVolume,
+  mid_gradeVolume: mid_gradeVolume,
+  mid_gradePurchasePrice: mid_gradePurchasePrice,
+  mid_gradeSellPrice: mid_gradeSellPrice,
+  mid_gradeNetProfit: (mid_gradeSellPrice - mid_gradePurchasePrice) * mid_gradeVolume,
+  premiumVolume: premiumVolume,
+  premiumPurchasePrice: premiumPurchasePrice,
+  premiumSellPrice: premiumSellPrice,
+  premiumNetProfit: (premiumSellPrice - premiumPurchasePrice) * premiumVolume
+}
+
+const callApiaddSalesEntry = async () => {
+  const url = serverURL + "/api/addSalesEntry";
+  console.log(url);
+
+  const response = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify(salesEntry),
+    headers:{
+      'Content-Type':"application/json"
+    }
+  });
+  const body = await response.json();
+  if (response.status !== 200) throw Error(body.message);
+  return body;
+}
 
   return (
     <ThemeProvider theme={theme}>
@@ -63,6 +105,8 @@ export default function SignUp() {
                     InputLabelProps={{
                     shrink: true,
                     }}
+                    value={reportingDate}
+                    onChange={(e) => setReportingDate(e.target.value)}
                     />
             </Grid>
             <Grid 
@@ -73,16 +117,22 @@ export default function SignUp() {
                     id = "Volume"
                     label = "Regular Volume (L)"
                     name = "Volume"
+                    value={regularVolume}
+                    onChange={(e) => setRegularVolume(e.target.value)}
                 />
                 <TextField
                     id = "Price"
                     label = "Purchase Price (L)"
                     name = "Price"
+                    value={regularPurchasePrice}
+                    onChange={(e) => setRegularPurchasePrice(e.target.value)}
                     />
                 <TextField
                     id = "Price"
                     label = "Sell Price (L)"
                     name = "Price"
+                    value={regularSellPrice}
+                    onChange={(e) => setRegularSellPrice(e.target.value)}
                     />
             </Grid>
             <Grid 
@@ -93,16 +143,22 @@ export default function SignUp() {
                     id = "Volume"
                     label = "Mid-Grade Volume (L)"
                     name = "Volume"
+                    value={mid_gradeVolume}
+                    onChange={(e) => setMid_GradeVolume(e.target.value)}
                 />
                 <TextField
                     id = "Price"
                     label = "Purchase Price (L)"
                     name = "Price"
+                    value={mid_gradePurchasePrice}
+                    onChange={(e) => setMid_GradePurchasePrice(e.target.value)}
                     />
                 <TextField
                     id = "Price"
                     label = "Sell Price (L)"
                     name = "Price"
+                    value={mid_gradeSellPrice}
+                    onChange={(e) => setMid_GradeSellPrice(e.target.value)}
                     />
             </Grid>
             <Grid 
@@ -113,16 +169,22 @@ export default function SignUp() {
                     id = "Volume"
                     label = "Supreme Volume (L)"
                     name = "Volume"
+                    value={premiumVolume}
+                    onChange={(e) => setPremiumVolume(e.target.value)}
                 />
                 <TextField
                     id = "Price"
                     label = "Purchase Price (L)"
                     name = "Price"
+                    value={premiumPurchasePrice}
+                    onChange={(e) => setPremiumPurchasePrice(e.target.value)}
                     />
                 <TextField
                     id = "Price"
                     label = "Sell Price (L)"
                     name = "Price"
+                    value={premiumSellPrice}
+                    onChange={(e) => setPremiumSellPrice(e.target.value)}
                     />
             </Grid>      
             <Button
@@ -130,6 +192,7 @@ export default function SignUp() {
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
+              onClick={callApiaddSalesEntry}
             >Submit
             </Button>
             
